c298411631d79ec7a18864fc72a636ec3aea7758
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-02-02 16:09:27 +0000

    first commit

---
+++ datatypelesson.py
@@ -0,0 +1,7 @@
+favoritesfood = input('what is your favorite food? ')
+print('your favorite food is '+ favoritesfood)
+print = output note 
+input =input note
+value=('ian')
+no upper case letters note 
+red bad yellow hey blue yay note 
