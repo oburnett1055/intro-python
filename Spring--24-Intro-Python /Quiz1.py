4c3c82fffc80cdbe5d170d033a80037b0dae8ae9
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-02-16 16:00:52 +0000

    feb16 quizz

---
+++ Spring--24-Intro-Python /Quiz1.py
@@ -0,0 +1,60 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user?( print will be a excellent built in function for output of data ) 
+
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?(input)
+
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. (complexity is a storage of a group code a box of cords example the human brain ,abstraction is a simplified organized code for friendly to the average person example a subway suffers)
+
+#4. What does it mean when you encounter a syntax error?(that your python writing grammer is incorect )
+
+#5. What is a variable? Provide an example of a variable in code. (a varible is a cup for the liquid of data)
+
+#6. Write a function in Python that when called will print out 
+# your name.  
+
+#7. What is a function definition and a function call? (a function definition is a name/title of a funtion a function call is when you call on a function on command)
+
+#8. Describe what each symbol means?
+
+# = // single equal
+# == // double equal
+# != // exclaimation equal
+
+#9. What will the output for the following code block be?3+('3')
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+3+('3')
+
+#10. What will the output for the following code block be? [20, 40, 60, 80]
+
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.
+
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?(to see true or false example ('cat or dog')
+
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+
+# Bonus : What does the type() function do?
---
+++ warmupfeb14.py
@@ -0,0 +1,13 @@
+a buit in function is a secondary tool builit into one with py for example is a preinstalled like print
+
+a computer has a built in funtion such as a clock and we have used type which helps sends back what data type its is 
+
+data that can be in a python list is a list of data[cheese,cat,hat]
+
+def =definition is the title of the function 
+( is basicaly the start
+)is the end of a listb of funtion 
+
+to use a funtion write snake
+def=Cheese()(
+print('cheesburgar')) to call this type Cheese()    
