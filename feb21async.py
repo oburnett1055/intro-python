fad07525bb004c0e230439c26566c82495c1d8f8
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-02-22 16:18:58 +0000

    FEB21VID ASSSIGHMENT

---
+++ feb21async.py
@@ -0,0 +1,13 @@
+#based on the video you just watched, in your own words, describe what is the Socratic Method. socratic method is a method of questioning what may seem to be a conclusive answer
+
+ #Why do you think it is important to asks questions in software engineering? to find any flaws in ones code before it happens 
+
+ #Based on the video you just watched, in your own words, what do you think a data broker is?
+
+# Why do you think companies collect and sell people's data?
+
+ #Based on the video you just watched, what does structural systemic racism mean? Provide 1 (one) example of structural system racism.
+
+# Why do you think it is important to build algorithms where there exists racial context?
+
+ #Why do you think it is important to have people of color involved in the development of these AI algorithms and systems?
