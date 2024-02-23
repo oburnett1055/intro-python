8b61d1cfd1db536bc5cacd003f73c5f6edd723b0
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-02-23 16:25:00 +0000

    feb23 warmup/notes

---
+++ warmupfeb23.py
@@ -0,0 +1,22 @@
+#to create a function two parts are required a function def () and a call
+
+#a function parmeter is part of a function inside the def () and a function argument is the action of this parmeter the input to the end 
+
+#== is a comparison operator which means its the same thing cake==bread
+
+def NameAge(Name,Age):
+   Age=Age+10
+print('hi {Name}your{Age} will be {Age} in 10 years ') 
+NameAge(Name=10)  
+
+#else is a built in function it is used for if like if a password is wrong or right if the choose is incorrect the fuction will swicth from if to else which is the plan b to a false input      
+#elif is a plan b for  the plan b 
+
+def omartest(cheese,cat):
+    cheese=100
+    cat=20
+    cheese==cat
+    if cheese==cat:
+        print('hi welcom to my test')
+
+#tab is indent 
