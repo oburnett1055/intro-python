055b949155c13c5a8141aebeed02ee487fb1bffb
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-03-07 16:18:48 +0000

    march7 class review

---
+++ March7ClassReviewNotes.py
@@ -0,0 +1,8 @@
+# a program is a set of intructions for computers 
+#programing languages are differenty ways to provide instruction
+#a computer program is a set of instructions that act based on certain conditions
+#py is a way we talk to computers called code
+#py syntax is the grammer of coding                                                                      
+#data types are tools of definition symbols that have word like kanji in japanese string,int,float,booblean,print,input
+#py has a specific language 
+#varibles are boxes that we can store data in cheese==cat that use cammle ,snake and p something i forgot
