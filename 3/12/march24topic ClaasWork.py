9047a87cff9c18a984bc09588deffa1caabf39e7
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-03-12 15:25:01 +0000

    march12 class talk

---
+++ 3/12/march24topic ClaasWork.py
@@ -0,0 +1,17 @@
+# Programming Questions
+# How do you think technology has influenced and
+# shaped how you learn in school? Do you think it's
+# been positive or negative? Please explain why 
+# by providing examples, written in 4 to 5 complete 
+# sentences. 
+'technology has set a positive wave on education but with that wave comes the sleep of the sand with most abusing or not using their time to their fullest.
+"I have personaly seen this in my self and others.Phones, tablets games distract from the riches of tech.School inputing tech in their work has led to effientcy but at the cost of disconnection with students-
+"in some situations has led to students becoming their own teacher.This inclusive tech generation has led to phisical success but some or mayby man to mental stress.conclusion tech is good for some but not all'
+# What are some ways you think technology can help 
+# people learn? Please explain why by providing examples,
+# written in 4 to 5 complete sentences. 
+/';'
+
+#WAY TECH CAN HELP PEOPLE SUCCEED
+#Tech can'nt help if people ar'nt willing to help them selfs.Technology is a tool it can'nt mold something thats not there like it can'nt help a person thats not there.
+#Technology has been a tool to me for 6 years, a distration for 10 great tool bad teacher.
