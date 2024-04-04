00282cda48016c1c56bd0438ba51468b0f1b1770
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-04-04 15:19:49 +0000

    loop review

---
+++ april4warmup.py
@@ -0,0 +1,44 @@
+#warm  UP
+
+#A PYTHON Loop is a loop of one comand over and over again 
+
+# a iterator is a starting point for the loop the 1 then 2
+
+#to stop a loop 
+
+#msg='hello' #msg is being put with hello
+#while msg=='hello':#while is loop saying the is saying as long as messagge is true print hello
+   # print(msg)
+# to break it make the statement untrue
+
+#msg='hello'
+#while msg=='bye':
+    #print(msg)
+
+
+def drivethru():
+    welcome=input('what do you want to eat')
+    orders=[]
+    ordrcomplete=False
+    while ordrcomplete==False:
+        ordrcomplete=input('what else')
+        if orders=='y':
+            print('what')
+        elif orders=='n':#break stops thed loop
+              welcome+=orders
+drivethru()
+
+#to surmarize your education today you learned the loop you learnd how to make a list that compare to a other list i
+                                                                                                                  #i
+                                                                                                                  #v
+                                                                                                              #example 
+                                                                                                    item=[]
+                                                                                                    price=[]
+#                                                                                              then to pare them together i have to use the index number rule as well
+                                                                                            #as = and other varibles to hive it def example
+                                                                                            if item==0 and price==0
+                                                                                              #note to self i got lost but this did interest me
+
+
+                                                                                              #today we used input alot with if and elif that helped combine a result in the 
+                                                                                              #function today
