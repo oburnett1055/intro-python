986a31853f0835d1425904e1ec2f79fd7ea0407b
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-04-08 14:19:15 +0000

    april8 confusion and loops

---
+++ April8.py
@@ -0,0 +1,14 @@
+# 1. In your own words, describe what a while loop is?(a while loop is a statement that will coutinue to print as long as the statement is true)
+
+#2. Create a function that uses a while loop to determine if a user has typed in 
+# the the correct word guess. If the user types in the wrong word, your program 
+# should inform them that their guess was inccorect and to try again, but if the
+# user guesses the word correctly, your program shoul tell the user they have 
+# guessed correctly and have won the game, stopping the loop.
+
+code='yes'
+while code!='no':
+        print('wrong code bob')
+        coder=input("whats the code bob")
+if coder=='no':
+        code='no'
