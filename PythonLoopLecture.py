42ddf0099a612c4fdde24511728dc803955dce4a
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-04-12 15:25:59 +0000

    loops april 12 2024

---
+++ PythonLoopLecture.py
@@ -0,0 +1,27 @@
+#a for loop is a is loop that will only stay consitent for a list different from a whie loop that will be infinite for a true statement
+
+#i can use while loops for passcode and for actions that might require repedictive commands
+
+#i can use for loops for task that require specific scans of a list /order 
+
+
+#numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#for x in numbers:
+   # if x==4:
+       # print('4 is even')
+
+
+
+me=input('mer')
+while me=='yes':
+    print('no')
+    if me=='maybe':
+        print('good')
+    else:
+        print('cheese')
+
+
+
+        #look up status codes=errors/flauses
+
+
