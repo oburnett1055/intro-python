ef8b291f11fe6f463bc03218f33fae604c4cc7af
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-05-03 15:23:31 +0000

    project app

--- async_mondayApril29.py
+++ async_mondayApril29.py
@@ -8,8 +8,43 @@
 
 
 
-def tictacto():
-    board=int['1|2|3|\n|4|5|6|\n|7|8|9|']
-    print(board(3))
-tictacto()
+# def tictacto():
+#    board='1|2|3|\n|4|5|6|\n|7|8|9|'
+#    print(board)
+# tictacto()
 
+topRow = [0,0,0]
+middleRow = [0,0,0]
+bottomRow = [0,0,0]
+
+board = [topRow,middleRow,bottomRow]
+print(f'{topRow} \n {middleRow} \n {bottomRow} \n')
+
+
+
+
+
+
+
+
+topRow = [0,0,0]
+middleRow = [0,0,0]
+bottomRow = [0,0,0]
+
+board = [topRow,middleRow,bottomRow]
+print(f'{topRow} \n {middleRow} \n {bottomRow} \n')
+
+
+
+ai==topRow,middleRow,bottomRow 
+
+
+
+
+#step one:create data set rules for data 
+#step two
+#step three
+#step four
+#step five
+#step six
+#step saeven
---
+++ bigAppProject.py
@@ -0,0 +1,8 @@
+# my app will solve the problem of the lack of debating apps
+#it will have a tiktok art design 
+#and a voting section on each post
+#will have a limit of online users
+#and will use cammera and videos fo online post 
+#will have animations 
+#and a limit of post per profile
+#will have a comment section
