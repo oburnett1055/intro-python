e8681223c1586e5f7e8f33ffb2a556dadc7b018c
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-05-07 15:23:59 +0000

    big app

--- bigAppProject.py
+++ bigAppProject.py
@@ -1,8 +1,14 @@
-# my app will solve the problem of the lack of debating apps
-#it will have a tiktok art design 
-#and a voting section on each post
-#will have a limit of online users
-#and will use cammera and videos fo online post 
-#will have animations 
-#and a limit of post per profile
-#will have a comment section
+#nick name app 
+#this app will provide nick names to people like me need of a unique nick name based off of their personal traits and give them a unquie nick based off of 3 connective letters on their first name, 2
+#of their favorite colors ,activity using 3connective letters ,whats the favorite species (reptiles(scaly))(mamals(fury))(insects(tiny))
+
+
+
+#Ask - What problem are you trying to solve?(this app will provide nick names to people like me need of a unique nick name based off of their personal traits )
+#Research - What tools and resources can you use to help you build your program?(w3schools and google/youtube and my notes and cheese)
+#Imagine - Based on the research you've done, determine exactly what you want to build.(a fun nick name app to make people laugh)
+#plan - Create wire frames and a development plan for creating your program.
+#Create - Use your development plan and your resources to begin coding your program
+#Test - Test out your code, work out the errors.
+#Improve - Are there things you can add to your program to make function even better.
+#The design and development of your project will have three phases: design, development, presentation
