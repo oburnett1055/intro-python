a0029b7e68abbfd7e31999940c6d57aa8baf5728
oburnett1055 <158183802+oburnett1055@users.noreply.github.com>
2024-05-08 15:13:42 +0000

    Omar's app pitch

--- OmarAppPitch.py
+++ OmarAppPitch.py
@@ -1,14 +1,16 @@
-#nick name app 
-#this app will provide nick names to people like me need of a unique nick name based off of their personal traits and give them a unquie nick based off of 3 connective letters on their first name, 2
-#of their favorite colors ,activity using 3connective letters ,whats the favorite species (reptiles(scaly))(mamals(fury))(insects(tiny))
+#paragraph 1 idea
+
+#My app idea is to make a auto nick name generater.This app will print out a unique nick name based off of 4 factors (favorite ), name , hobby , sports, animals ).
+#based off the input of the user the app will then proceed to connect these 4 factors via random listings all indivisual that will relie on a len like built in function to scan the input for the 
+#lenght of the input from  1 to 10.The info from the len in put will all be formed in a unison form in a patten not in the view of the user but the coder using a random range 1 to 3 that will chose a different pattern base off the indivisual integer.
 
 
+#what problem does my app solve 
 
-#Ask - What problem are you trying to solve?(this app will provide nick names to people like me need of a unique nick name based off of their personal traits )
-#Research - What tools and resources can you use to help you build your program?(w3schools and google/youtube and my notes and cheese)
-#Imagine - Based on the research you've done, determine exactly what you want to build.(a fun nick name app to make people laugh)
-#plan - Create wire frames and a development plan for creating your program.
-#Create - Use your development plan and your resources to begin coding your program
-#Test - Test out your code, work out the errors.
-#Improve - Are there things you can add to your program to make function even better.
-#The design and development of your project will have three phases: design, development, presentation
+#my app will provide a quick and unique way for new socail media users to create a free fun longlasting nick name for their socail media accounts . These names can be applied to many social media accounts such as Tiktok,
+#IG, and Snapchat adding a consistentcy to your name and personality.The app has iterator functions that will allow you to use the app repeatidly til you find your name for your traits.
+#lastly its fun you might laugh at the name you recieve.
+
+#how will my app work
+
+#in the intro of the app it will ask you would you like to beggin.Then the app will ask you to input 4 personal traits of your choicing.The app app will then print your oder then ask if you would like to try again.The app will have a character limit of 10 characters. 
